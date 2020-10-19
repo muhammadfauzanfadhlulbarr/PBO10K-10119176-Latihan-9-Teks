0607dd53f9abd463cbd893717b828b66563b03a4
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-10-19 10:53:51 +0700

    commit

---
+++ Latihan9.java
@@ -0,0 +1,9 @@
+public class Latihan9 {
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab +"berisi " + c);
+    }
+}
